f71fd9eae44318663d1bd64a67c3e83710e7a9d4
AffumBenjamin <139506777+AffumBenjamin@users.noreply.github.com>
2023-07-28 13:26:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,22 @@ var listItens = document.querySelectorAll('.draggable');
 var imageIcon // moon or sun icon
 
 //----------------------------------
+function getPromise(URL) {
+  let promise = new Promise(function (resolve, reject) {
+    let req = new XMLHttpRequest();
+    req.open("GET", URL);
+    req.onload = function () {
+      if (req.status == 200) {
+        resolve(req.response);
+      } else {
+        reject("There is an Error!");
+      }
+    };
+    req.send();
+  });
+  return promise;
+}
+
 const POKEMONS_BAD_URL = 'https://pokeapi.co/api/v2/pokemon-bad/';
 
 // This will reject as the URL is 404
